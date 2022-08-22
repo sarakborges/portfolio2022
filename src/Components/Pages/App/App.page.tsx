56c84ca8383ffd78bf1e4e4c24a8997647a17b3b
Sara Borges <sarakborges@outlook.com>
2022-08-22 17:58:39 -0300

    initial thing

---
+++ src/Components/Pages/App/App.page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import * as Styled from './App.style'
+
+export const AppPage: FC = () => {
+  return (
+    <>
+      <Styled.Global />
+
+      <div>It was just a prank, bruv</div>
+    </>
+  )
+}
